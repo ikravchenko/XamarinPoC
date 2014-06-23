c76e94c0be8705ea951363c40bad49489a7db9c1
Ivan Kravchenko <ikravchenko@debonw43010.stacobon.dd13.mydorma.com>
2014-06-23 09:30:25 +0200

    fitting network test for async

--- Tests/NetworkTest.cs
+++ Tests/NetworkTest.cs
@@ -7,10 +7,20 @@ namespace Tests
 	[TestFixture]
 	public class NetworkTest
 	{
+		[SetUp]
+		public void PrepareData()
+		{
+			RequestData ();
+		}
+
 		[Test]
 		public void TestDBAfterRequest() {
-			new NetworkManager ().RequestAndSaveWeather ();
 			Assert.AreEqual (16, TaskManager.GetTasks ().Count);
+		}
+
+		public async void RequestData() {
+			var nr = new NetworkManager().RequestAndSaveWeather ();
+			await nr;
 		}
 	}
 }
